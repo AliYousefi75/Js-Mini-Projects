d5d4e6e2403055c88bcf3b6c205aa0c6a49b7616
ali <ali.yousefi7518@gmail.com>
2026-07-15 12:31:34 +0430

    Gravity(add variables)

--- 3.ball-with-gravity/script.js
+++ 3.ball-with-gravity/script.js
@@ -4,10 +4,28 @@ canvas.height = window.innerHeight
 
 let c = canvas.getContext('2d')
 
+this.screen = {
+    width:window.innerWidth,
+    height:window.innerHeight
+}
+
+this.mouse ={
+    x: screen.width /2,
+    y: screen.height /2
+}
 
 class Ball{
-    constructor(){
-
+    constructor(x, y, dx, dy, r, color){
+        this.gravity = 1 
+        this.friction =0.8 
+        this.r = r || 10      
+        this.x = x || randonIntFromInterval(0 + this.r, window.innerWidth - this.r)
+        this.y = y || randonIntFromInterval(0 + this.r, window.innerHeight - this.r)
+        this.dx = dx || (Math.random() - 0.5) * 10
+        this.dy = dy || (Math.random() ) * 20
+        this.color = color || `rgba (231 ,76 ,60 ,${Math.random()})`;
+        this.rc = "rgb(" + randonIntFromInterval(0, 255) + ',' + randonIntFromInterval(0, 255) + ',' + randonIntFromInterval(0, 255) + ")";
+        this.draw()
     }
 
     draw(){
