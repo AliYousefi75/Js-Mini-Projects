0b35ba2df6b99cdc473259f2b84af02e6b7554e2
ali <ali.yousefi7518@gmail.com>
2026-07-15 12:25:42 +0430

    Gravity(add base class)

---
+++ 3.ball-with-gravity/script.js
@@ -0,0 +1,59 @@
+let canvas = document.querySelector('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let c = canvas.getContext('2d')
+
+
+class Ball{
+    constructor(){
+
+    }
+
+    draw(){
+
+    }
+
+    update(){
+
+    }
+}
+
+class Canvas{
+
+    constructor(){
+
+    }
+
+    animate(){
+
+    }
+
+}
+
+let myVan = new Canvas()
+myCan.animate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function randonIntFromInterval(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
